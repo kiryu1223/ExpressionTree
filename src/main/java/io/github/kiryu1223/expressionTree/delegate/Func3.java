344166cca0175d1364fda4ce97a1f6d465e91026
kiryu1223 <1085684963@qq.com>
2024-06-17 23:32:14 +0800

    增加action和func到15

--- src/main/java/io/github/kiryu1223/expressionTree/delegate/Func3.java
+++ src/main/java/io/github/kiryu1223/expressionTree/delegate/Func3.java
@@ -1,6 +1,6 @@
 package io.github.kiryu1223.expressionTree.delegate;
 
-public interface Func3<T1,T2,T3,R> extends Delegate
+public interface Func3<T1, T2, T3, R> extends Delegate
 {
-    R invoke(T1 t1,T2 t2,T3 t3);
+    R invoke(T1 t1, T2 t2, T3 t3);
 }
